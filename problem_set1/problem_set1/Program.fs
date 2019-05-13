a199dff744c757dfcf380e480b607634651c4a59
Natalie Rodriguez <nrodr047@fiu.edu>
2019-05-13 15:20:05 -0400

    problem 2 complete

--- problem_set1/problem_set1/Program.fs
+++ problem_set1/problem_set1/Program.fs
@@ -8,10 +8,28 @@ let problem1()=
     printfn "1) ANSWER: C"
     printfn "  a). 2 + 5 * 10 = %d" (2 + 5 * 10)
     printfn "  b). 10I * 20I = %A" (10I + 20I)
-    printfn "  c). 4 + 5.6 - cannot add float and int because they are different types."
+    printfn "  c). 4 + 5.6 //cannot add float and int because they are different types."
     printfn "  d). Strings can be added = %s\n" ("4" + "5.6")
 
 problem1()
+
+let problem2()=
+    printfn "2). ANSWER: C"
+    printfn "  a). t1 * t2 -> t3 //is a tuple that returns something"
+    printfn "  b). t1 -> t2 * t3 //is a function that returns a tuple"
+    printfn "  c). t1 -> (t2 -> t3) //is the same as t1 -> t2 -> t3, which is curried form."
+    printfn "  d). (t1 -> t2) -> t3 //is a function that returns something\n"
+
+    let cadd a = fun b -> a + b
+    
+    printfn "   example:\n   cadd a = fun b -> a + b\n   (cadd 3) 7 = %A \n   int -> (int -> int)\n" ((cadd 3) 7)
+
+problem2()
+
+//let problem3()=
+
+//problem3()
+
 
 
 
