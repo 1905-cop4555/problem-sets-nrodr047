bd618ca502b182507e25062dab8a7e83b089d479
Natalie Rodriguez <nrodr047@fiu.edu>
2019-05-23 21:44:46 -0400

    Problem 1 - figuring out how to use curried and uncurried functions with examples

--- problem_set2/problem_set2/Program.fs
+++ problem_set2/problem_set2/Program.fs
@@ -1,7 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+open System
+
+let problem1() =
+
+    //TUPLE SIGNATURE: ('a * 'b) -> 'c
+    //CURRIED SIGNATURE: ('a -> 'b -> 'c)
+
+    //takes a curried function f x y and tuples them to f(x,y)
+    let uncurry f = (fun x y -> f(x,y))
+    // value: ('a * 'b -> c) -> ('a -> 'b -> 'c)
+
+    //takes a uncurried function and curries them
+    let curry f = (fun(x,y)->f x y)
+    // value: ('a -> 'b -> 'c) -> ('a * 'b -> 'c)
+
+    let plus = curry (+)
+    plus (2,3)
+
+
+
+
+problem1()
+
+Console.ReadKey() |> ignore
